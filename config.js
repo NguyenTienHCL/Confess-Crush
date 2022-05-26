ba168dc566f3b02c19017301f8ed8d76d630b289
NguyenTienHCL <101309897+NguyenTienHCL@users.noreply.github.com>
2022-05-26 15:21:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Gửi cho ... :3",
-    introTitle: 'Tí hon xấu tính hay mách bố',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hì hì =)))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Gửi cho Phương ú nu :3",
+    introTitle: 'Đồ ú nu đáng ghéc -.-',
+    introDesc: `Câu thả thính là tớ copy đó, nhưng tình cảm dành cho cậu là thật lòng =)))`,
+    btnIntro: 'hì hì',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy "cái má phúng phính" ấy ',
     btnYes: 'Thích lắm <3',
     btnNo: 'Không nha :3',
-    question: 'Cho mừn xin cốc trà đào  <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Cho mừn xin cắn má 1 miếng <3',
+    btnReply: 'Gửi cho Tiến <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Cậu thi xong đi chơi 1 chuyến nhó !!!!',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.instagram.com/_pudding.2000/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/pudding.2610' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
